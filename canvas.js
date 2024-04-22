3dff7a572f89180b142a39a2037c6f66e22a33ba
Artyom Lysenko <tema199532384@gmail.com>
2024-04-22 16:39:32 +0300

    Removed Memory Leaks (I hope)

--- canvas.js
+++ canvas.js
@@ -56,7 +56,6 @@ class Utils {
         './Images/12.jpeg', 
         './Images/13.jpeg', 
         './Images/14.jpeg', 
-        './Images/15.jpeg', 
         './Images/19.jpeg',
         './Images/18.jpeg', 
         './Images/16.jpeg', 
@@ -69,17 +68,14 @@ class Utils {
         './Images/27.jpeg', 
         './Images/28.jpeg', 
         './Images/29.jpeg', 
-        './Images/31.jpg', 
-        './Images/32.jpg', 
+        './Images/31.jpg',  
         './Images/33.jpg',
-        './Images/34.jpg', 
         './Images/37.jpeg', 
         './Images/38.jpeg', 
         './Images/39.jpeg', 
         './Images/40.jpeg', 
         './Images/41.jpeg', 
-        './Images/42.jpeg',
-        './Images/43.png'
+        './Images/42.jpeg'
     ]
 
     static soundTrack
@@ -190,45 +186,61 @@ class Canvas {
     static canvasWidth = Utils.canvasWidth
     static canvasHeight = Utils.canvasHeight
     static filledCoordsMap = new Map()
-    static filledCoordsSorted = []
     static keyDirections = { 37: 'left', 38: 'up', 39: 'right', 40: 'down'}
-    static currentFigure
-    static nextFigure = FigureForms.getRandomFigure()
+    static currentFigureCoords
+    static nextFigureCoords = FigureForms.getRandomFigure()
     static nextColor = FigureForms.getRandomColor()
     static currentColor
     static points = 0
     static level = 1
     static startSpeed =  1000
     static SPEED = this.startSpeed
+    static currentFigure
 
     static initializeFigure() {
-        this.currentFigure = this.nextFigure
-        this.nextFigure = FigureForms.getRandomFigure()
+
+        if (this.currentFigure) {
+            this.currentFigure.removeListeners()
+            this.currentFigure.activeFigure = null
+            this.currentFigure.nextFigureCoords = null
+            clearInterval(this.currentFigure.quickMoveInterval)
+            cancelAnimationFrame(this.currentFigure.requestAnimationFrameId)
+            this.currentFigure = null
+        }
+        
+        this.currentFigureCoords = this.nextFigureCoords
+        this.nextFigureCoords = FigureForms.getRandomFigure()
         this.currentColor = this.nextColor
         this.nextColor = FigureForms.getRandomColor()
-        const type = FigureForms.allFiguresMap.get(this.currentFigure)
+        const type = FigureForms.allFiguresMap.get(this.currentFigureCoords)
         const startOffset = Utils.getRandomStartCoord(type)
-        const newFigureCoords = this.currentFigure.map(obj => {
+        const newFigureCoords = this.currentFigureCoords.map(obj => {
             return {x : obj.x + startOffset, y: obj.y}
         })
-        const figure = new Figure(newFigureCoords, this.nextFigure, type, this.currentColor, this.nextColor)
-        figure.initializeListener()
-        figure.moveDown()
+        this.currentFigure = new Figure(newFigureCoords, this.nextFigureCoords, type, this.currentColor, this.nextColor)
+        this.currentFigure.initializeListener()
+        this.currentFigure.moveDown()
+        this.currentFigure.calculateShadowCoords()
+        Canvas.childCtx.clearRect(0,0,130,100)
+        this.currentFigure.drawNextFigure()
     }
 }
 
 class Figure {
 
     figurePosition = 'right'
-    startAnimationTime = null
+    startAnimationTime
     createNewFigure = false
     direction
-    quickMoveInterval = null
+    quickMoveInterval
+    requestAnimationFrameId
     pressTimer
     activeFigure
     nextFigureCoords
     type
     color
+    boundKeyUp
+    boundHeyDown
 
     constructor(newFigureCoords, nextFigureCoords, type, color, nextColor) {
         this.type = type
@@ -236,11 +248,18 @@ class Figure {
         this.nextFigureCoords = nextFigureCoords
         this.color = color
         this.nextColor = nextColor
+        this.boundHeyDown = this.moveFigure.bind(this)
+        this.boundKeyUp = this.handleKeyUp.bind(this)
+    }
+
+    removeListeners() {
+        document.removeEventListener('keydown', this.boundHeyDown);
+        document.removeEventListener('keyup', this.boundKeyUp);
     }
 
     initializeListener() {
-        document.addEventListener('keydown', this.moveFigure.bind(this))
-        document.addEventListener('keyup', this.handleKeyUp.bind(this))
+        document.addEventListener('keydown', this.boundHeyDown)
+        document.addEventListener('keyup', this.boundKeyUp)
     }
    
     moveFigure(event) {
@@ -250,20 +269,24 @@ class Figure {
         else if (Canvas.keyDirections[eventKeyCode] === 'left') this.direction = 'left'
         else if (Canvas.keyDirections[eventKeyCode] === 'up') this.direction = 'up'
         else if (Canvas.keyDirections[eventKeyCode] === 'down') this.direction = 'down'
-        this.changeFigureCoords(this.direction)
+
+        if (this.changeFigureCoords(this.direction)) {
+            this.playQuickMoveAudio()
+        }
+        
         if (this.direction !== 'up' && !this.pressTimer) {
             this.pressTimer = setTimeout(function() {
                 context.quickMoveInterval = context.direction === 'down' ? context.quickMove(20) : context.quickMove(40)
             }, 25)
         }
-        this.playQuickMoveAudio()
+       
     }
 
     playQuickMoveAudio() {
         Utils.quickMoveSound?.stop();
         Utils.quickMoveSound = new Howl({
             src: ['sounds/quick_10.mp3'],
-            volume : .5
+            volume : .1
         });
         Utils.quickMoveSound.play();
     }
@@ -281,9 +304,8 @@ class Figure {
     quickMove(speed) {
         clearInterval(this.quickMoveInterval)
         return setInterval(() => {
-            this.changeFigureCoords(this.direction)
-            if (this.direction !== 'down') {
-                //this.playQuickMoveAudio()
+            if (this.changeFigureCoords(this.direction) && this.direction !== 'down') {
+                this.playQuickMoveAudio()
             }
         }, speed)
     }
@@ -449,6 +471,7 @@ class Figure {
             if (!this.collisionCheck(coordsCopy)) {
                 this.figurePosition = newPosition
                 this.assignActiveFigure(coordsCopy)
+                this.calculateShadowCoords()
             }
         }
     }
@@ -462,8 +485,6 @@ class Figure {
 
     redrawCanvas() {
         Canvas.ctx.clearRect(0,0, Canvas.canvasWidth, Canvas.canvasHeight)
-        Canvas.childCtx.clearRect(0,0,130,100)
-        this.drawNextFigure()
         this.drawFallenFigures()
         this.drawScore()
         this.drawFigure()
@@ -495,12 +516,11 @@ class Figure {
                     Canvas.filledCoordsMap.set(figure, this.color)
                 })
                 this.activeFigure.length = 0
-                Canvas.filledCoordsSorted = Array.from(Canvas.filledCoordsMap.keys()).sort((a,b) => a.y - b.y)
                 this.tetrisCheck()
                 Canvas.initializeFigure()
             }
         } else {
-            requestAnimationFrame(() => this.moveDown())
+            this.requestAnimationFrameId = requestAnimationFrame(() => this.moveDown())
         }
     }
 
@@ -538,40 +558,47 @@ class Figure {
             this.assignActiveFigure(coordsCopy)
             allowChangeCoords = true
         }
+        if (direction === 'left' || direction === 'right') {
+            this.calculateShadowCoords()
+        }
         return allowChangeCoords
     }
 
     drawShadowFigure() {
-        const shadowCoords = this.calculateShadowCoords()
-        if (shadowCoords[0].y > this.activeFigure[0].y + Utils.edgeSize) {
-            shadowCoords.forEach(coord => {
+        if (this.shadowFigure[0]?.y > this.activeFigure[0].y + Utils.edgeSize) {
+            this.shadowFigure.forEach(coord => {
                 Canvas.ctx.fillStyle = 'rgba(225, 215, 220, 0.5)';
                 Canvas.ctx.fillRect(coord.x, coord.y, Utils.edgeSize, Utils.edgeSize);
             });
         } 
     }
 
+    shadowFigure = []
+
     calculateShadowCoords() {
         const shadowCoords = this.activeFigure.map(figure => {
             return {...figure}
         })
-        while(!this.collisionCheck(shadowCoords)) {
+        let counter = 0
+        while(!this.collisionCheck(shadowCoords) && counter < 20) {
+            counter++
             shadowCoords.forEach(coord => coord.y += Utils.edgeSize)
         }
         shadowCoords.forEach(coord => coord.y -= Utils.edgeSize)
-        return shadowCoords
+        this.shadowFigure = shadowCoords
     }
 
     collisionCheck(nextFigurePositions) {
+        const filledCoords = Array.from(Canvas.filledCoordsMap.keys())
         for (let i = 0; i < nextFigurePositions.length; i++) {
             if (nextFigurePositions[i].y > Canvas.canvasHeight - Utils.edgeSize) {
                 return true
             } else if (nextFigurePositions[i].x < 0 || nextFigurePositions[i].x > Canvas.canvasWidth - Utils.edgeSize) {
                 return true
-            } else if (Canvas.filledCoordsSorted.length > 0) {
-                for (let j = 0; j < Canvas.filledCoordsSorted.length; j++) {
-                    if (Canvas.filledCoordsSorted[j].y === nextFigurePositions[i].y && 
-                        Canvas.filledCoordsSorted[j].x === nextFigurePositions[i].x) {
+            } else if (filledCoords.length > 0) {
+                for (let j = 0; j < filledCoords.length; j++) {
+                    if (filledCoords[j].y === nextFigurePositions[i].y && 
+                        filledCoords[j].x === nextFigurePositions[i].x) {
                         return true
                     }
                 }
@@ -631,8 +658,6 @@ class Figure {
             }, totalFuntTime);
             this.playRemoveLinesAudio()
         }
-
-        Canvas.filledCoordsSorted = Array.from(Canvas.filledCoordsMap.keys()).sort((a,b) => a.y - b.y)
         Canvas.points += this.calculatePoints(moveDownFor)
         Canvas.level = (Math.ceil(Canvas.points / 5000) === 0 ? 1 : Math.ceil(Canvas.points / 5000))
         Canvas.SPEED = Canvas.startSpeed - (50 * (Canvas.level - 1))
@@ -649,6 +674,7 @@ class Figure {
 }
 
 function startGame() {
+    //Canvas.createDemoFallenFigures()
     Utils.shuffle(Utils.music)
     Canvas.initializeFigure()
     setTimeout(() => {
